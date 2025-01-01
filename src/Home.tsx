c61e8be1ece0e08452eff7c8a546733000b7c9b8
Rony <mrrohan192@gmail.com>
2025-01-01 17:34:58 +0600

    the push sortablefavoriteapp

--- src/Home.tsx
+++ src/Home.tsx
@@ -52,7 +52,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 5,
     overflow: 'hidden',
-    marginTop: StatusBar.currentHeight,
+    // marginTop: StatusBar.currentHeight,
     justifyContent: 'flex-end',
   },
 })
